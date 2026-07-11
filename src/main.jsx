7329294d44e989f9cd1e9002fdc21196ce1990e7
Deepak Erothi <deepakerothi7@gmail.com>
2026-07-11 12:58:03 +0530

    Add React Router v7 future flags to silence warnings

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Lewis-Animal-Hospital">
+    <BrowserRouter basename="/Lewis-Animal-Hospital" future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
