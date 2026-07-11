06d08bf2ff1b79a6b032ce290c3e4a2e8ca85750
Deepak Erothi <deepakerothi7@gmail.com>
2026-07-11 13:05:42 +0530

    Fix missing PawPrint import in Navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useMemo } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { PawPrint } from 'lucide-react';
 import { motion } from 'framer-motion';
 import clsx from 'clsx';
 
